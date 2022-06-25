e33efe6ac4e3f518d4a05428bf3770392624c905
Mark <73297926+markrachapoom@users.noreply.github.com>
2022-06-25 17:07:11 +0700

    Deploy smart contract on Goerli network, LET'S GOOO🦄🎉

---
+++ config/index.ts
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+// Configuration
+export const config: any = {
+    INFURA_ID: process.env.INFURA_ID,
+    GOERLI_PRIVATE_KEY: process.env.GOERLI_PRIVATE_KEY
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,18 @@
 require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
+
+const INFURA_ID = process.env.INFURA_ID;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.7.3",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_ID}`,
+      accounts: [`${GOERLI_PRIVATE_KEY}`]
+    }
+  }
 };
